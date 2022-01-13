d79d57798e4ab215697903b80ec5fd3d9031321e
Sandra-Be <sandraberzk@gmail.com>
2022-01-13 15:28:44 +0000

    Added wishlist add functionality

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_wishlist, name='view_wishlist')
+    path('', views.wishlist, name='wishlist')
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from profiles.models import UserProfile
+from products.models import Product
+
+from django.contrib import messages
+from .models import Wishlist
 
 # Create your views here.
 
-def view_wishlist(request):
+def wishlist(request):
     """ A view that renders the wishlist contents page """
 
+    wishlist = None
+    try:
+        wishlist = Wishlist.objects.get(user=request.user)
+    except Wishlist.DoesNotExist:
+        pass
+
+    context = {
+        'wishlist': wishlist,
+    }
+
     return render(request, 'wishlist/wishlist.html')
+
+
+def add_to_wishlist(request, product_id):
+    """
+    Add a product from the store to the
+    wishlist
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+    wishlist, _ = Wishlist.objects.get_or_create(user=request.user)
+    wishlist.products.add(product)
+    messages.info(request, 'A new product was added to your wishlist')
+
+    return redirect(redirect_url)
