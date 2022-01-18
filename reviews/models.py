7e0c6725521325afdad1dedf4ee0743592419d33
Sandra-Be <sandraberzk@gmail.com>
2022-01-18 23:31:42 +0000

    Fixed error

--- reviews/models.py
+++ reviews/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-from .models import Product
+from products.models import Product
 
 
 class Review(models.Model):
     """ Model for reviews """
     username = models.ForeignKey(User, on_delete=models.CASCADE,
                                  null=True, blank=True)
-    name = models.CharField(max_length=254)
+    name = models.ForeignKey(Product, on_delete=models.CASCADE)
     title = models.CharField(max_length=254, null=True, blank=True)
     body = models.CharField(max_length=1000)
     firstcreated = models.DateTimeField(auto_now_add=True)
     lasteditted = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return str(self.product)
+        return str(self.name)
