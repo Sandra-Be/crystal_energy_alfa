db8502bbda7e1b6706d8b3eecee69ca1a1819933
Sandra-Be <sandraberzk@gmail.com>
2022-01-18 20:12:44 +0000

    Added review forms

---
+++ reviews/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from . models import Review
+
+
+class ReviewForm(forms.ModelForm):
+
+    class Meta:
+        model = Review
+        fields = ['product', 'title', 'body', ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
