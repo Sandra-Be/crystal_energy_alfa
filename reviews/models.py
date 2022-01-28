78c5456d7fac064c1a42b845341cabfa8887e318
Sandra-Be <sandraberzk@gmail.com>
2022-01-28 20:06:23 +0000

    Removed image_url

--- reviews/models.py
+++ reviews/models.py
@@ -12,7 +12,7 @@ class Review(models.Model):
     body = models.CharField(max_length=1000)
     firstcreated = models.DateTimeField(auto_now_add=True)
     lasteditted = models.DateTimeField(auto_now=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+#    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
