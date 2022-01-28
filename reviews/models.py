ad2986d6283e582d85869c672ae73334b5f90cd8
Sandra-Be <sandraberzk@gmail.com>
2022-01-28 20:09:22 +0000

    Removed image variable

--- reviews/models.py
+++ reviews/models.py
@@ -13,7 +13,7 @@ class Review(models.Model):
     firstcreated = models.DateTimeField(auto_now_add=True)
     lasteditted = models.DateTimeField(auto_now=True)
 #    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+#    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return str(self.name)
